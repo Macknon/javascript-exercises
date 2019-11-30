942b65d85d68a2aacb70583b775db8054d7279bf
Injenga Macknon Korongo <macknoninjenga2000@gmail.com>
2019-11-30 11:37:49 +0300

    add changes

--- 4-standard-lib/strings.js
+++ 4-standard-lib/strings.js
@@ -1,123 +1,496 @@
 /**
+
+
  * ⚠️☝️ INSTRUCTIONS ☝️⚠️
+
+
  *
+
+
  * - Do not remove any code
+
+
  * - Only add or edit code in areas marked with ✏️
+
+
  */
+
+
+
+
+
 describe("strings", function() {
+
+
     "use strict";
+
+
+
+
+
     describe("Find index of string partial", function() {
+
+
         it("should run without errors", function() {
+
+
             const text = "The quick brown fox jumps over the lazy dog";
 
+
+
+
+
             ////////////////////////////////////////////////////////////////////
+
+
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
 
-            function findIndexOf(string) {
+
+            function findIndexOf(string){
+
+
                 return text.indexOf(string);
 
+
             }
+
+
             ////////////////////////////////////////////////////////////////////
 
+
+
+
+
             expect(findIndexOf("fox")).to.equal(16);
+
+
             expect(findIndexOf("dog")).to.equal(40);
+
+
         });
+
+
     });
+
+
+
+
+
     describe("Find last index of string partial", function() {
+
+
         it("should run without errors", function() {
+
+
             const text = "The quick brown fox jumps over the lazy dog";
+
+
+
+
+
             ////////////////////////////////////////////////////////////////////
+
+
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
+
+
+            function findLastIndexOf(string){
+
+
+                return text.lastIndexOf(string);
+
+
+            }
+
+
             ////////////////////////////////////////////////////////////////////
+
+
+
+
+
             expect(findLastIndexOf("e")).to.equal(33);
+
+
             expect(findLastIndexOf("a")).to.equal(36);
+
+
         });
+
+
     });
+
+
+
+
+
     describe("Get letter at certain position in string", function() {
+
+
         it("should run without errors", function() {
+
+
             const text = "The quick brown fox jumps over the lazy dog";
+
+
+
+
+
             ////////////////////////////////////////////////////////////////////
+
+
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
+
+
+            function letterAt(string){
+
+
+                return text.charAt(string);
+
+
+            }
+
+
             ////////////////////////////////////////////////////////////////////
+
+
+
+
+
             expect(letterAt(0)).to.equal("T");
+
+
             expect(letterAt(4)).to.equal("q");
+
+
             expect(letterAt(10)).to.equal("b");
+
+
             expect(letterAt(-1)).to.equal("");
+
+
         });
+
+
     });
+
+
+
+
+
     describe("Begins with substring", function() {
+
+
         it("should run without errors", function() {
+
+
             const text = "The quick brown fox jumps over the lazy dog";
+
+
+
+
+
             ////////////////////////////////////////////////////////////////////
+
+
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
+
+
+            function beginsWith(string){
+
+
+                return text.includes(string);
+
+
+            }
+
+
             ////////////////////////////////////////////////////////////////////
+
+
+
+
+
             expect(beginsWith("The")).to.equal(true);
+
+
             expect(beginsWith("The quick brown fox")).to.equal(true);
+
+
             expect(beginsWith(text)).to.equal(true);
+
+
             expect(beginsWith("")).to.equal(true);
+
+
             expect(beginsWith("The lazy dog")).to.equal(false);
+
+
         });
+
+
     });
+
+
+
+
+
     describe("Remove whitespace around string", function() {
+
+
         it("should run without errors", function() {
+
+
             ////////////////////////////////////////////////////////////////////
+
+
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
+
+
+            const text = "   example sentence   "
+
+
+            function removeWhitespaceAround(string){
+
+
+                return text.trim(string);
+
+
+            }
+
+
             ////////////////////////////////////////////////////////////////////
+
+
+
+
+
             expect(removeWhitespaceAround("   example sentence   ")).to.equal(
+
+
                 "example sentence"
+
+
             );
+
+
             expect(removeWhitespaceAround("\n\texample sentence\n\t")).to.equal(
+
+
                 "example sentence"
+
+
             );
+
+
         });
+
+
     });
+
+
+
+
+
     describe("String contains substring", function() {
+
+
         it("should run without errors", function() {
+
+
             const text = "The quick brown fox jumps over the lazy dog";
+
+
+
+
+
             ////////////////////////////////////////////////////////////////////
+
+
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
+
+
+            function contains(string){
+
+
+                return text.includes(string);
+
+
+            }
+
+
             ////////////////////////////////////////////////////////////////////
+
+
+
+
+
             expect(contains("quick")).to.equal(true);
+
+
             expect(contains("fox")).to.equal(true);
+
+
             expect(contains("dog")).to.equal(true);
+
+
             expect(contains("Quick")).to.equal(false);
+
+
             expect(contains("Fox")).to.equal(false);
+
+
             expect(contains("hound")).to.equal(false);
+
+
         });
+
+
     });
+
+
+
+
+
     describe("Get substring from string", function() {
+
+
         it("should run without errors", function() {
+
+
             const text = "The quick brown fox jumps over the lazy dog";
+
+
+
+
+
             ////////////////////////////////////////////////////////////////////
+
+
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
+
+
+            function cut(number,x){
+
+
+                return text.slice(number,x);
+
+
+            } 
+
+
             ////////////////////////////////////////////////////////////////////
+
+
+
+
+
             expect(cut(0)).to.equal(
+
+
                 "The quick brown fox jumps over the lazy dog"
+
+
             );
+
+
             expect(cut(1)).to.equal(
+
+
                 "he quick brown fox jumps over the lazy dog"
+
+
             );
+
+
             expect(cut(-1)).to.equal("g");
+
+
             expect(cut(-2)).to.equal("og");
+
+
             expect(cut(0, 0)).to.equal("");
+
+
             expect(cut(0, 1)).to.equal("T");
+
+
             expect(cut(0, -1)).to.equal(
+
+
                 "The quick brown fox jumps over the lazy do"
+
+
             );
+
+
             expect(cut(1, -1)).to.equal(
+
+
                 "he quick brown fox jumps over the lazy do"
+
+
             );
+
+
         });
+
+
     });
+
+
+
+
+
     describe("Transform to capital letters", function() {
+
+
         it("should run without errors", function() {
+
+
             const text = "The quick brown fox jumps over the lazy dog";
+
+
+
+
+
             ////////////////////////////////////////////////////////////////////
+
+
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
+
+
+
+
+
+            function transformToCapitalLetters(string){
+
+
+                return text.toUpperCase(string);
+
+
+            }
+
+
             ////////////////////////////////////////////////////////////////////
+
+
+
+
+
             expect(transformToCapitalLetters()).to.equal(
+
+
                 "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+
+
             );
+
+
         });
+
+
     });
+
+
 });
